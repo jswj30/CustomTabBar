afe6308ad0e8f564dff25e9a375aa5a8a324d7ca
jswj <jswj30@naver.com>
2022-10-06 10:38:29 +0900

    animation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useReducer} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Pressable,
+  LayoutChangeEvent,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {
   BottomTabBarProps,
@@ -7,8 +14,18 @@ import {
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+// svg
+import Svg, {Path} from 'react-native-svg';
+// reanimated
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+  useDerivedValue,
+} from 'react-native-reanimated';
 
 const Tab = createBottomTabNavigator();
+
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 const App = () => {
   return (
@@ -30,10 +47,105 @@ const PlaceholderScreen = () => {
   return <View style={{flex: 1, backgroundColor: '#604AE6'}} />;
 };
 
-const AnimatedTabBar = (props: BottomTabBarProps) => {
+const AnimatedTabBar = ({
+  state: {index: activeIndex, routes},
+  navigation,
+  descriptors,
+}) => {
   const {bottom} = useSafeAreaInsets();
 
-  return <View style={[styles.tabBar, {paddingBottom: bottom}]}></View>;
+  // get information about the components position on the screen ------
+
+  const reducer = (state: any, action: {x: number; index: number}) => {
+    return [...state, {x: action.x, index: action.index}];
+  };
+
+  const [layout, dispatch] = useReducer(reducer, []);
+  console.log(layout);
+
+  const handleLayout = (event: LayoutChangeEvent, index: number) => {
+    dispatch({x: event.nativeEvent.layout.x, index});
+  };
+
+  const xOffset = useDerivedValue(() => {
+    if (layout.length !== routes.length) {
+      return 0;
+    }
+    return [...layout].find(({index}) => index === activeIndex)!.x - 25;
+  }, [activeIndex, layout]);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: withTiming(xOffset.value, {duration: 250})}],
+    };
+  });
+
+  return (
+    <View style={[styles.tabBar, {paddingBottom: bottom}]}>
+      <AnimatedSvg
+        width={110}
+        height={60}
+        viewBox="0 0 110 60"
+        style={[styles.activeBackground, animatedStyles]}>
+        <Path
+          fill="#604AE6"
+          d="M20 0H0c11.046 0 20 8.954 20 20v5c0 19.33 15.67 35 35 35s35-15.67 35-35v-5c0-11.046 8.954-20 20-20H20z"
+        />
+      </AnimatedSvg>
+
+      <View style={styles.tabBarContainer}>
+        {routes.map((route, index) => {
+          const active = index === activeIndex;
+
+          return (
+            <TabBarComponent
+              key={route.key}
+              active={active}
+              onLayout={e => handleLayout(e, index)}
+              onPress={() => navigation.navigate(route.name)}
+            />
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+type TabBarComponentProps = {
+  active?: boolean;
+  onLayout: (e: LayoutChangeEvent) => void;
+  onPress: () => void;
+};
+
+const TabBarComponent = ({active, onLayout, onPress}: TabBarComponentProps) => {
+  const animatedComponentCircleStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withTiming(active ? 1 : 0, {duration: 250}),
+        },
+      ],
+    };
+  });
+
+  const animatedIconContainerStyles = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(active ? 1 : 0.5, {duration: 250}),
+    };
+  });
+
+  return (
+    <Pressable onPress={onPress} onLayout={onLayout} style={styles.component}>
+      <Animated.View
+        style={[styles.componentCircle, animatedComponentCircleStyles]}
+      />
+      <Animated.View
+        style={[styles.iconContainer, animatedIconContainerStyles]}>
+        {/* @ts-ignore */}
+        <Text>?</Text>
+      </Animated.View>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
